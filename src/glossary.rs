4dbbeb1b3b1c341e8019ab9b502f72badacf2df9
Calvin Brown <cab025@protonmail.com>
2020-09-22 11:10:21 -0500

    Adding glossary and initial client

---
+++ src/glossary.rs
@@ -0,0 +1,41 @@
+use crate::NwsClient;
+use serde::{Deserialize, Serialize};
+use anyhow::Result;
+
+#[derive(Debug)]
+#[derive(Deserialize, Serialize)]
+pub struct NwsGlossaryResponse {
+    pub glossary: Vec<NwsGlossaryInfo>,
+}
+
+#[derive(Debug)]
+#[derive(Deserialize, Serialize)]
+pub struct NwsGlossaryInfo {
+    pub term: String,
+    pub definition: String,
+}
+
+pub trait NwsGlossary {
+    fn get_glossary_terms(&self) -> Result<NwsGlossaryResponse>;
+}
+
+impl NwsGlossary for NwsClient {
+    fn get_glossary_terms(&self) -> Result<NwsGlossaryResponse> {
+        let host = format!("{}/glossary", &self.host);
+        let nws_alert: NwsGlossaryResponse = self.client.get(&host).send()?.json()?;
+        Ok(nws_alert)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::NwsClient;
+    use crate::glossary::NwsGlossary;
+
+    #[test]
+    fn it_works() {
+        let client = NwsClient::new(None);
+        println!("{:?}", client.get_glossary_terms());
+        assert_eq!(2 + 2, 3);
+    }
+}
